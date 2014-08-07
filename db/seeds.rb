064a731a45d1215da14fd2e355e4e68ff25c3de3
Jason Zhang <jason.zhang@marketboomer.com>
2014-08-07 18:37:10 +0800

    add fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if ENV['RAILS_ENV'] && %w"production demo staging".include?(ENV['RAILS_ENV'].downcase)
+  puts "Cannot seed base data to #{ENV['RAILS_ENV']} environment"
+else
+  Dir.glob("#{Rails.root}/db/fixtures/*.yml").each do |file|
+    puts "Loading base data for: #{File.basename(file, '.*')}"
+    ActiveRecord::FixtureSet.create_fixtures("#{Rails.root}/db/fixtures", File.basename(file, '.*'))
+  end
+end
