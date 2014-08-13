1ff274f3d458fca58353897e2bf0871d474a9874
Jason Zhang <jason.zhang@marketboomer.com>
2014-08-13 12:58:15 +0800

    add paperclip and config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ActiveRecord::Base
   validates_presence_of :email, :name, :passwd
 
+  has_attached_file :avatar,
+  :default_style => :normal,
+  :style => {
+    :small => "16x16#",
+    :normal => "48x48#",
+    :large => "80>",
+  },
+  :url =>"#{APP_CONFIG['upload_url']}/:class/:attachment/:hashed_path/:id_:style.jpg",
+  :path => "#{APP_CONFIG['upload_root']}/:class/:attachment/:hashed_path/:id_:style.jpg",
+  :default_url => "avatar/:style.jpg"
+
   STATE = {
       :normal => 1,
       #屏蔽
       :blocked => 2
   }
+
 end
---
+++ config/initializers/paperclip_hashpath.rb
@@ -0,0 +1,8 @@
+#扩展paperclip加入hash目录 ：hash_path
+require "digest"
+ Paperclip.interpolates :hashed_path do |attachment, style|
+   hash = Digest::MD5.hexdigest(attachment.instance.id.to_s)
+   hash_path = ''
+   3.times {hash_path += '/' + hash.slice!(0..2)}
+   hash_path[1..12]
+ end
